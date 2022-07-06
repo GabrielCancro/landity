b5226c95b9ff97d281e349a13ad5015e2f469c45
gabrielcancro <gabrielcancro@3ogs.com.ar>
2022-07-06 12:20:00 -0300

    update

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameSystem : MonoBehaviour
 {
-    static GameObject selectedLight;
+     static GameObject selectedLight;
+     static GameObject buildPanel;
      static GameObject currentSelected;
 
     public static void init()
     {
         if (selectedLight) return;
         selectedLight = GameObject.Find("SelectedLight");
+        buildPanel = GameObject.Find("BuildPanel");
     }
     public static void onClickElemnt(GameObject GO)
     {
@@ -22,7 +24,10 @@ public class GameSystem : MonoBehaviour
             Debug.Log(selectedLight);
             var pos = currentSelected.transform.position;
             selectedLight.transform.position = new Vector3(pos.x,.1f,pos.z);
-        }
+	if ( GO.tag == "Build" ) buildPanel.SetActive(true);
+	else buildPanel.SetActive(false);        
+	}
+	
         
     }
 }
